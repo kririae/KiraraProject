0da45ccee24cf94d991dca2d05ff36bd160bc34b
kririae <kriaeth@outlook.com>
2024-10-11 13:44:50 +0800

    .

--- kirara-dance/Scene/SceneBuilder.h
+++ kirara-dance/Scene/SceneBuilder.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "Core/KIRA.h"
+#include "Core/Object.h"
+
 namespace krd {
+class Scene;
+
 /// The facade to construct the scene through different configurations, e.g.,
 /// raw TOML, glTF, etc.
 ///
@@ -14,5 +19,14 @@ public:
     ~SceneBuilder() = default;
 
 public:
+    /// Load the scene from a file.
+    ///
+    /// \throw kira::Anyhow if the file is not found or the file is not a valid scene configuration.
+    /// \throw std::exception if any other error occurs.
+    void loadFromFile(std::filesystem::path const &path);
+
+public:
+    /// Build the scene.
+    [[nodiscard]] Ref<Scene> build() const;
 };
 } // namespace krd
